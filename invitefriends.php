13918a009c13bb37b150a190b9f8185bf56c378e
Victor Nwauwa <victor.nwauwa93@gmail.com>
2017-10-25 15:59:16 -0700

    Add files via upload

---
+++ invitefriends.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/html">
+<head>
+    <meta charset="UTF-8">
+    <title>Pay Bills</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="css/invitefriend.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+</head>
+<body>
+    <div class="container con">
+        <div class="row ">
+            <div class="invite-head col-sm-8 col-xs-8 ">
+                <h3 class="panel-heading">Invite friends</h3>
+            </div>
+        </div>
+
+        <div class="row ">
+        <div class="  invite-body col-sm-8 col-xs-8 ">
+            <div class="row text ">
+                <h3>MAKE YOUR TRANSACTIONS FUN</h3>
+                <p>Earn 5% of what we charge you at the end of the month</p>
+                <p>Find out more</p>
+            </div>
+            <div class="row">
+                <div class="refcode">
+                <h5>Referal code</h5>
+                </div>
+                <div>
+                    <form>
+                        <input id="invitebox" type="text" name="username" placeholder="enter name">
+                        <button type="submit" value="send" class="btn" id="invite-btn">Send</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+        </div>
+     </div>
+</body>
+</html>
